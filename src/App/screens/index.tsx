732ebbc623faaecf82e7a14b6b5ff9f442a3014f
Ankur Seth <iankurseth@gmail.com>
2024-10-26 11:57:08 +0530

    Updated vite.config.ts

--- src/App/screens/index.tsx
+++ src/App/screens/index.tsx
@@ -1,2 +1,2 @@
 
-export { default as MainScreen } from "./MainScreen/MainScreen";
+
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler' // or "modern"
+			}
+		}
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
